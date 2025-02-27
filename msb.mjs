c9848d04cbf6103e65874c5ca79ee0dec3401585
Markus Bopp <markus.bopp@trac.network>
2025-02-27 09:58:48 +0100

    # appending "transaction" through tx channel

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ import MainSettlementBus from './src/index.js';
 const store = new Corestore('stores/' + process.argv[2])
 
 const opts = {
-    bootstrap: '7ef1fab8ddff5fbfd9aac7c52a8543f81ec87060a4efe2f682adadfcecb9a7b5',
+    bootstrap: '5bab30f8d1839aece31f55d4ead3de108f86ba6b756bebea30e284eeb7d23f58',
     channel: Buffer.alloc(32).fill('0000tracnetworkmainsettlementbus'),
     tx : Buffer.alloc(32).fill('00tracnetworkmainsettlementbustx')
 };
