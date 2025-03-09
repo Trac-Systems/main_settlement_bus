aa4de2d45c5163c45295bd02df3dc27639477dc6
Benny <uniftyio@gmail.com>
2025-03-09 15:46:09 +0100

    b9d08a60(refactoring to not use MSB hyperbee for the remote MSB channel and various bugsquashing based results)

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ import MainSettlementBus from './src/index.js';
 const store = new Corestore('stores/' + process.argv[2])
 
 const opts = {
-    bootstrap: '24fe13135fa09749ea2c046eb10f2e1008830dfd6e88f71fec27ff39c6fdf8bb',
+    bootstrap: '9612ff5717f29afa489bf83cd0b0a5764b2fd433af754d7b1495103b5495fa14',
     channel: Buffer.alloc(32).fill('00botracnetworkmainsettlementbus'),
     tx : Buffer.alloc(32).fill('botracnetworkmainsettlementbustx')
 };
