07312373e0ad7c284a761c9f71eaf891c48d7f1a
b-garbacz <bartlomiej.garbacz@trac.network>
2025-03-21 14:40:09 +0100

    restore config

--- msb.mjs
+++ msb.mjs
@@ -2,9 +2,9 @@ import MainSettlementBus from './src/index.js';
 const opts = {
     stores_directory : 'stores/',
     store_name : process.argv[2],
-    bootstrap: 'a0b246a66f93d0fda58e661a51d0a17f4fa457aa03040dc26c1292c77f1853eb',
+    bootstrap: '3ad0f48d685fdf10c551e596c48596a99ff373c65845d8827da8b7862a476979',
     channel: Buffer.alloc(32).fill('00botracnetworkmainsettlementbus'),
-    tx : Buffer.alloc(32).fill('1botracnetworkmainsettlementbustx')
+    tx : Buffer.alloc(32).fill('botracnetworkmainsettlementbustx')
 };
 
 const msb = new MainSettlementBus(opts);
