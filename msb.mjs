fb81890407fdf82665d546ff716944f941b99373
Benny <uniftyio@gmail.com>
2025-03-22 12:36:27 +0100

    version bump

--- msb.mjs
+++ msb.mjs
@@ -2,7 +2,7 @@ import MainSettlementBus from './src/index.js';
 const opts = {
     stores_directory : 'stores/',
     store_name : process.argv[2],
-    bootstrap: '3ad0f48d685fdf10c551e596c48596a99ff373c65845d8827da8b7862a476979',
+    bootstrap: '8e192d111aee4a29497f2967a4126fdabc2071dc7885a62a82bdfc97b4e4a2e3',
     channel: Buffer.alloc(32).fill('00botracnetworkmainsettlementbus'),
     tx : Buffer.alloc(32).fill('botracnetworkmainsettlementbustx')
 };
--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { createHash } from "node:crypto";
+
 //TODO: if something is missing, add additonal sanitization
 // parsed.op === 'pre-tx' -> moved out of the scope this check because we can re-use this function in the apply
 // TODO: Split sanitization on pre and post TX
@@ -15,68 +15,11 @@ export function sanitizeTransaction(parsedTx) {
     );
 }
 
-export function restoreManifest(parsedManifest) {
-
-    if (Array.isArray(parsedManifest.signers)) {
-        parsedManifest.signers = parsedManifest.signers.map(signer => {
-            if(signer.namespace && signer.namespace.data &&signer.publicKey && signer.publicKey.data){ 
-                return {
-                    ...signer,
-                    namespace: Buffer.from(signer.namespace.data),
-                    publicKey: Buffer.from(signer.publicKey.data),
-                }
-            } else {
-                return signer;
-            }
-        });
+export async function addWriter(input, peer){
+    const splitted = input.split(' ');
+    if(splitted[0] === '/add_writer'){
+        await peer.base.append({ type: 'addWriter', key: splitted[splitted.length - 1] });
+    } else if(splitted[0] === '/add_writer2') {
+        await peer.base.append({ type: 'addWriter2', key: splitted[splitted.length - 1] });
     }
-
-    return parsedManifest;
-}
-
-// To improve - could be done in better approach (simplify)
-export function restoreHash(parsedPreTx) {
-    const reconstructedContentHash = createHash('sha256')
-        .update(JSON.stringify(parsedPreTx.ch))
-        .digest('hex');
-
-    const reconstructedTxHash = createHash('sha256')
-        .update(
-            parsedPreTx.w + '-' +
-            parsedPreTx.i + '-' +
-            parsedPreTx.ipk + '-' +
-            reconstructedContentHash + '-' +
-            parsedPreTx.in
-        )
-        .digest('hex');
-
-    const finalReconstructedTxHash = createHash('sha256')
-        .update(reconstructedTxHash)
-        .digest('hex');
-    return finalReconstructedTxHash;
-}
-
-export async function verifyDag(autoBaseInsance) {
-    try {
-        console.log('--- DAG Monitoring ---');
-        const dagView = await autoBaseInsance.view.core.treeHash();
-        const lengthdagView = autoBaseInsance.view.core.length;
-        const dagSystem = await autoBaseInsance.system.core.treeHash();
-        const lengthdagSystem = autoBaseInsance.system.core.length;
-        console.log('this.base.view.core.signedLength:', autoBaseInsance.view.core.signedLength);
-        console.log("this.base.signedLength", autoBaseInsance.signedLength);
-        console.log("this.base.linearizer.indexers.length", autoBaseInsance.linearizer.indexers.length);
-        console.log("this.base.indexedLength", autoBaseInsance.indexedLength);
-        console.log(`base.key/writingKey: ${autoBaseInsance.key.toString('hex')}`);
-
-        console.log(`VIEW Dag: ${dagView.toString('hex')} (length: ${lengthdagView})`);
-        console.log(`SYSTEM Dag: ${dagSystem.toString('hex')} (length: ${lengthdagSystem})`);
-
-    } catch (error) {
-        console.error('Error during DAG monitoring:', error.message);
-    }
-}
-
-export async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
 }
