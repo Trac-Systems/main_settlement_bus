cb66f410c0199b1f7d1c36030063385aa28f7072
Benny <uniftyio@gmail.com>
2025-04-04 20:10:04 +0200

    version bumps

--- msb.mjs
+++ msb.mjs
@@ -3,7 +3,7 @@ import MainSettlementBus from './src/index.js';
 const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
-    bootstrap: '75cfdf69c717ae551ad53c370c64cbe957437488f00304a796295becd4c94eca',
+    bootstrap: 'db83156cbf7d227ad08fc4117fc247f387c15311e3597d65c2a09a768e97d70a',
     channel: '00bptracnetworkmainsettlementbus',
     tx : 'bptracnetworkmainsettlementbustx'
 };
