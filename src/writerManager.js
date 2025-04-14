82f2f53568c2a2e54b3d62a839acc337682875d0
Benny <uniftyio@gmail.com>
2025-04-14 17:03:18 +0200

    replaced all occurences of getSigned with get as some of these are blocking and not necessary as signed

--- src/writerManager.js
+++ src/writerManager.js
@@ -13,7 +13,7 @@ export class WriterManager extends ReadyResource {
 
     async addAdmin() {
         // case where admin entry doesn't exist yet and we have to autorize Admin public key only with bootstrap writing key
-        const adminEntry = await this.msbInstance.getSigned('admin');
+        const adminEntry = await this.msbInstance.get('admin');
         if (!adminEntry && this.msbInstance.writingKey && this.msbInstance.writingKey === this.msbInstance.bootstrap) {
 
             const nonce = Math.random() + '-' + Date.now();
@@ -44,7 +44,7 @@ export class WriterManager extends ReadyResource {
         try {
             //TODO: IMPORTANT -  IF WE GONNA STORE ~ 2K-10K PUBLIC KEYS IN THE LIST, WE NEED TO SPLIT IT INTO CHUNKS
             // ONE CHUNK WILL BE ~100 PUBLIC KEYS + NONCE + SIG AND ADDITIONAL BYTES < 4096 BYTES. ADMIN WILL NEED TO PERFORM MULTIPLE APPENDS. FOR NOW THIS IS NOT IMPLEMENTED.
-            const adminEntry = await this.msbInstance.getSigned('admin');
+            const adminEntry = await this.msbInstance.get('admin');
             
             if (adminEntry && this.msbInstance.wallet.publicKey === Buffer.from(adminEntry.tpk.data).toString('hex')) {
             
