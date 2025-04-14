1dcefead2fb3780bcc5f026eaeb19d99e8c0b720
Benny <uniftyio@gmail.com>
2025-04-14 19:44:48 +0200

    enforcing connection.destroy on errors

--- src/network.js
+++ src/network.js
@@ -148,7 +148,7 @@ class Network {
                         }
                     }catch(e){
                         console.log(e);
-                        try{ await connection.destroy(); }catch (e){}
+                        try{ await connection.destroy(); } catch (e){ }
                     }
                 });
                 connection.on('close', () => { });
