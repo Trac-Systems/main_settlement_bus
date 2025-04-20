4518f58302aa9bae499c3e2471b5e82ba98137c2
Benny <uniftyio@gmail.com>
2025-04-20 23:17:10 +0200

    internal dev release

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
     bootstrap: '4577ebf1e84c06d48b2caa33fe54ae58be3a79cb30810129d58b725fae412e37',
-    channel: '00tracnetworkmainsettlementbusr1'
+    channel: '00axtracnetworkmainsettlementbus'
 };
 
 const msb = new MainSettlementBus(opts);
