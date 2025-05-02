2fe79a451f1deef5f25dbc9509dfd2df11c73633
Benny <uniftyio@gmail.com>
2025-05-02 23:26:34 +0200

    added null address to prevent install collisions

--- msb.mjs
+++ msb.mjs
@@ -3,8 +3,8 @@ import {MainSettlementBus} from './src/index.js';
 const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
-    bootstrap: 'd7cce91547144be2e234992501b556dcd1591ab44313cf3036ddd3137afbc347',
-    channel: '00axtracnetworkmainsettlementbus',
+    bootstrap: '0000000000000000000000000000000000000000000000000000000000000000',
+    channel: '000000000000000mainsettlementbus',
     enable_txlogs : true,
 };
 
