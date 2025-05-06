b8e97e38727909f87287cfcf5b0c64ca4c27c20d
Benny <uniftyio@gmail.com>
2025-05-06 17:14:14 +0200

    fixed core "bloat"

--- msb.mjs
+++ msb.mjs
@@ -5,6 +5,8 @@ const opts = {
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
     bootstrap: '0768953b234c79eccc6306fdcba2d7c1f0b05b9af6815a3502e96a83a8878ff7',
     channel: '00axtracnetworkmainsettlementbus',
+    disable_rate_limit : true,
+    enable_txlogs : true
 };
 
 const msb = new MainSettlementBus(opts);
