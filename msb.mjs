59571150d77c35d72d7d9b346f6a3563d54ebf5c
Benny <uniftyio@gmail.com>
2025-05-10 00:17:45 +0200

    reduced updater interval

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
     bootstrap: '0768953b234c79eccc6306fdcba2d7c1f0b05b9af6815a3502e96a83a8878ff7',
-    channel: '00axtracnetworkmainsettlementbus',
+    channel: '00abtracnetworkmainsettlementbus',
     disable_rate_limit : true,
     enable_txlogs : true
 };
