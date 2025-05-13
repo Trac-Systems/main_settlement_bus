286dfcdebeb4a9f8c40618c81ae9c40d8f248282
b-garbacz <bartlomiej.garbacz@trac.network>
2025-05-13 12:16:02 +0200

    hotfix - replace custom `is_hex_string` rule with built in string+length+custom as hexStringCheck validation

--- src/utils/check.js
+++ src/utils/check.js
@@ -1,6 +1,6 @@
 import Validator from 'fastest-validator';
 import { isHexString } from './functions.js';
-import {OperationType} from './constants.js';
+import { OperationType } from './constants.js';
 class Check {
     #_validator;
     #_sanitizeAdminAndWritersOperations;
@@ -67,14 +67,14 @@ class Check {
         const schema = {
             $$strict: true,
             type: { type: 'string', enum: [OperationType.ADD_ADMIN, OperationType.ADD_WRITER, OperationType.REMOVE_WRITER], required: true },
-            key: { type: "is_hex_string", length: 64, required: true },
+            key: { type: "string", length: 64, required: true, custom: 'hexStringCheck' },
             value: {
                 $$strict: true,
                 $$type: "object",
-                pub: { type: 'is_hex_string', length: 64, required: true },
-                wk: { type: 'is_hex_string', length: 64, required: true },
-                nonce: { type: 'is_hex_string', length: 64, required: true },
-                sig: { type: 'is_hex_string', length: 128, required: true },
+                pub: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                wk: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                nonce: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                sig: { type: 'string', length: 128, required: true, custom: 'hexStringCheck' },
 
             }
         }
@@ -90,12 +90,12 @@ class Check {
         const schema = {
             $$strict: true,
             type: { type: 'string', enum: [OperationType.ADD_INDEXER, OperationType.REMOVE_INDEXER, OperationType.APPEND_WHITELIST, OperationType.BAN_VALIDATOR], required: true },
-            key: { type: "is_hex_string", length: 64, required: true },
+            key: { type: "string", length: 64, required: true, custom: 'hexStringCheck' },
             value: {
                 $$strict: true,
                 $$type: "object",
-                nonce: { type: 'is_hex_string', length: 64, required: true },
-                sig: { type: 'is_hex_string', length: 128, required: true },
+                nonce: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                sig: { type: 'string', length: 128, required: true, custom: 'hexStringCheck' },
 
             }
         }
@@ -111,15 +111,15 @@ class Check {
         const schema = {
             $$strict: true,
             op: { type: 'string', enum: ['pre-tx'], required: true },
-            tx: { type: 'is_hex_string', required: true }, // TODO: if we will use only 256 bit hash then change to length: 64
-            is: { type: 'is_hex_string', length: 128, required: true },
-            wp: { type: 'is_hex_string', length: 64, required: true },
-            i: { type: 'is_hex_string', length: 64, required: true },
-            ipk: { type: 'is_hex_string', length: 64, required: true },
-            ch: { type: 'is_hex_string', required: true }, // TODO: if we will use only 256 bit hash then change to length: 64
-            in: { type: 'is_hex_string', length: 64,  required: true },
-            bs: { type: 'is_hex_string', length: 64, required: true },
-            mbs: { type: 'is_hex_string', length: 64, required: true },
+            tx: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            is: { type: 'string', length: 128, required: true, custom: 'hexStringCheck' },
+            wp: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            i: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            ipk: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            ch: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            in: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            bs: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+            mbs: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
         };
         return this.#_validator.compile(schema);
     }
@@ -133,23 +133,23 @@ class Check {
         const schema = {
             $$strict: true,
             type: { type: 'string', enum: ['tx'], required: true },
-            key: { type: 'is_hex_string', required: true }, // TODO: if we will use only 256 bit hash then change to length: 64
+            key: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
             value: {
                 $$strict: true,
                 $$type: "object",
                 op: { type: 'string', enum: ['post-tx'], required: true },
-                tx: { type: 'is_hex_string', required: true }, // TODO: if we will use only 256 bit hash then change to length: 64
-                is: { type: 'is_hex_string', length: 128, required: true },
-                w: { type: 'is_hex_string', length: 64, required: true },
-                i: { type: 'is_hex_string', length: 64, required: true },
-                ipk: { type: 'is_hex_string', length: 64, required: true },
-                ch: { type: 'is_hex_string', required: true }, // TODO: if we will use only 256 bit hash then change to length: 64
-                in: { type: 'is_hex_string', length: 64, required: true },
-                bs: { type: 'is_hex_string', length: 64, required: true },
-                mbs: { type: 'is_hex_string', length: 64, required: true },
-                ws: { type: 'is_hex_string', length: 128, required: true },
-                wp: { type: 'is_hex_string', length: 64, required: true },
-                wn: { type: 'is_hex_string', length: 64, required: true }
+                tx: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                is: { type: 'string', length: 128, required: true, custom: 'hexStringCheck' },
+                w: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                i: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                ipk: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                ch: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                in: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                bs: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                mbs: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                ws: { type: 'string', length: 128, required: true, custom: 'hexStringCheck' },
+                wp: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' },
+                wn: { type: 'string', length: 64, required: true, custom: 'hexStringCheck' }
             }
         };
         return this.#_validator.compile(schema);
