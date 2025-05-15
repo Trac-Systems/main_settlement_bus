7096073e5e2d46be56ebf5ba54e67dc435836c2d
leonardostsouza <leonardo.souza@trac.network>
2025-05-15 15:25:21 +0200

    Refactor msgUtils tests

--- test/msgUtils/createMessage.test.js
+++ test/msgUtils/createMessage.test.js
@@ -6,7 +6,7 @@ test('createMessage', async (t) => {
     const expected = b4a.alloc(32);
     for (let i = 0; i < expected.length; i++) {
         expected[i] = i;
-    } 
+    }
     const arg1 = expected.subarray(0, 8);
     const arg2 = expected.subarray(8, 16);
     const arg3 = expected.subarray(16, 24);
@@ -36,12 +36,12 @@ test('createMessage', async (t) => {
     });
 
     // TODO: This tests is working on node but not on bare. Investigate and reactivate
-    // t.test('creat'eMessage - function throws an error when invalid arguments are passed', async (k) => {
+    // t.test('createMessage - function throws an error when invalid arguments are passed', async (k) => {
     //     try {
     //         const res = MsgUtils.createMessage(1, 2, 3, 4);
     //         k.fail('Should throw an error for invalid arguments. Returned: ' + res);
     //     } catch (e) {
     //         k.pass('Correctly threw an error for invalid arguments');
     //     }
-    // });'
+    // });
 });
