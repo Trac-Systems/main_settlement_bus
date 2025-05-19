f61897b668dfd99b7bf7f06c5ef7ae80d99705c8
leonardostsouza <leonardo.souza@trac.network>
2025-05-19 12:22:53 +0200

    Fix isHexString function and update test cases accordingly

--- test/functions/isHexString.test.js
+++ test/functions/isHexString.test.js
@@ -2,9 +2,11 @@ import test from 'brittle';
 import { isHexString } from '../../src/utils/functions.js';
 
 test('isHexString', (t) => {
-    // t.ok(isHexString('0x1234567890abcdef'), 'Valid hex string should return true'); // Deactivated. See TODO in functions.js
-    t.ok(isHexString('1234567890abcdef'), 'Valid hex string should return true');
-    t.ok(isHexString('1234567890xyz') === false, 'Invalid hex string should return false');
-    t.ok(isHexString('123456789') === false, 'Invalid size hex string should return false');
-    // t.ok(isHexString('') === false, 'Empty string should return false'); // Deactivated. See TODO in functions.js
+    t.ok(isHexString('1234567890abcdef'), 'Valid hex string should return true with all lowercase characters');
+    t.ok(isHexString('1234567890ABCDEF'), 'Valid hex string should return true with all uppercase characters');
+    t.ok(isHexString('1234567890AbCdEf'), 'Valid hex string should return true with mixed case characters');
+    t.is(isHexString('0x1234567890abcdef'), false, '0x prefixed hex string should return false');
+    t.is(isHexString('1234567890xyz'), false, 'Invalid hex string should return false');
+    t.is(isHexString('123456789'), false, 'Invalid size hex string should return false');
+    t.is(isHexString(''), false, 'Empty string should return false');
 });
