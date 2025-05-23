b97f3c75c85724d7e57296b765939f17e327f142
leonardostsouza <leonardo.souza@trac.network>
2025-05-23 16:18:16 +0200

    Implement common setup for apply tests

--- test/apply/addWriter.test.js
+++ test/apply/addWriter.test.js
@@ -1,2 +1,21 @@
+import test from 'brittle';
+import {initMsbAdmin, initMsbPeer} from './setupApplyTests.js';
+import PeerWallet from "trac-wallet"
+import { testKeyPair1, testKeyPair2 } from '../fixtures/apply.fixtures.js';
+ 
  // handleApplyAddWriterOperation  and addWriter
  // should not be possible to perform replay attack
+
+ test('Apply function addWriter - happy path', async (t) => {
+    try {
+        const admin = await initMsbAdmin(testKeyPair1);
+        const writer = await initMsbPeer('writer', testKeyPair2, admin.options);
+
+        await admin.msb.ready();
+        await writer.msb.ready();
+    }
+    catch (error) {
+        t.fail(error.message);
+    }
+
+ });
---
+++ test/fixtures/apply.fixtures.js
@@ -0,0 +1,11 @@
+export const testKeyPair1 = {
+    publicKey: '82f6c1f684f4e251dfe092155b8861a0625b596991810b2b80b9c65ccbec5ad3',
+    secretKey: '734aa8a4ff1506a502054f537c235d3fbe70452926bad869c3ab57e90d06df7382f6c1f684f4e251dfe092155b8861a0625b596991810b2b80b9c65ccbec5ad3',
+    mnemonic: 'slight wedding permit mention subject mask hawk awkward sniff leopard spider scatter close neutral deny apple wide category sick love sorry pupil then legal'
+}
+
+export const testKeyPair2 = {
+    publicKey: "8f17ec2862c7b3e4dbbedaf88a68a9b74d76e9d6acca6ca4a5fafb1b6c474616",
+    secretKey: "21b7f3c56eaa4d8114530258c79b8086bcca3e61d6c9edee589e8ca2f48688e98f17ec2862c7b3e4dbbedaf88a68a9b74d76e9d6acca6ca4a5fafb1b6c474616",
+    mnemonic: "century category maze cover student upset trip cup purchase area turtle keen minimum flee diagram romance stool absorb umbrella phone valve avocado fade window"
+}
