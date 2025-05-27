bfa978cef01e9666bb95ff9d6b195a63dbf4a908
leonardostsouza <leonardo.souza@trac.network>
2025-05-27 18:51:47 +0200

    Implement basic tests for add and remove writers

--- test/apply/addWriter.test.js
+++ test/apply/addWriter.test.js
@@ -1,21 +1,42 @@
 import test from 'brittle';
-import {initMsbAdmin, initMsbPeer} from './setupApplyTests.js';
-import PeerWallet from "trac-wallet"
+import { tick, setupAdmin, setupMsbPeer, setupWhitelist } from './setupApplyTests.js';
 import { testKeyPair1, testKeyPair2 } from '../fixtures/apply.fixtures.js';
- 
- // handleApplyAddWriterOperation  and addWriter
- // should not be possible to perform replay attack
+import MsgUtils from '../../src/utils/msgUtils.js';
+import { sleep } from '../../src/utils/functions.js';
 
- test('Apply function addWriter - happy path', async (t) => {
+test('Apply function addWriter - happy path', async (t) => {
     try {
-        const admin = await initMsbAdmin(testKeyPair1);
-        const writer = await initMsbPeer('writer', testKeyPair2, admin.options);
+        const admin = await setupAdmin(testKeyPair1, {});
+        const writer = await setupMsbPeer('writer', testKeyPair2, admin.options);
 
-        await admin.msb.ready();
-        await writer.msb.ready();
+        // set up whitelist
+        const whitelistKeys = [writer.wallet.publicKey]
+        await setupWhitelist(admin, whitelistKeys);
+
+        const req = await MsgUtils.assembleAddWriterMessage(
+            writer.wallet,
+            writer.msb.writingKey,
+        );
+
+        // add writer to base
+        await admin.msb.base.append(req); // Send `add writer` request to apply function
+        await tick();
+        await sleep(5000); // wait for both peers to sync state
+        const result = await writer.msb.get(req.key); // check if the writer entry was added successfully in the base
+
+        // release resources
+        await writer.msb.close();
+        await admin.msb.close();
+
+        // check the result
+        t.ok(result, 'Result should not be null');
+        t.is(result.pub, writer.wallet.publicKey, 'Result pub should match writer public key');
+        t.is(result.wk, writer.msb.writingKey, 'Result writing key should match writer writing key');
+        t.ok(result.isWriter, 'Result should indicate that the peer is a valid writer');
+        t.is(result.isIndexer, false, 'Result should not indicate that the peer is an indexer');
     }
     catch (error) {
         t.fail(error.message);
     }
 
- });
+});
--- test/apply/removeWriter.test.js
+++ test/apply/removeWriter.test.js
@@ -1,2 +1,51 @@
-//handleApplyRemoveWriterOperation and removeWriter
-// should not be possible to perform replay attack
+import test from 'brittle';
+import { tick, setupAdmin, setupMsbPeer, setupWhitelist } from './setupApplyTests.js';
+import { testKeyPair1, testKeyPair2 } from '../fixtures/apply.fixtures.js';
+import MsgUtils from '../../src/utils/msgUtils.js';
+import { sleep } from '../../src/utils/functions.js';
+
+test('Apply function removeWriter - happy path', async (t) => {
+    try {
+        const admin = await setupAdmin(testKeyPair1, {});
+        const writer = await setupMsbPeer('writer', testKeyPair2, admin.options);
+
+        // set up whitelist
+        const whitelistKeys = [writer.wallet.publicKey]
+        await setupWhitelist(admin, whitelistKeys);
+
+        const reqAddWriter = await MsgUtils.assembleAddWriterMessage(
+            writer.wallet,
+            writer.msb.writingKey,
+        );
+
+        // add writer to base
+        await admin.msb.base.append(reqAddWriter); // Send `add writer` request to admin apply function
+        await tick();
+
+        // request writer removal
+        const reqRemoveWriter = await MsgUtils.assembleRemoveWriterMessage(
+            writer.wallet,
+            writer.msb.writingKey,
+        );
+
+        await admin.msb.base.append(reqRemoveWriter);
+        await tick();
+        await sleep(5000); // wait for both peers to sync state
+        const resultRemoveWriter = await writer.msb.get(reqRemoveWriter.key); // check if the writer entry was removed successfully in the base
+
+        // release resources
+        await writer.msb.close();
+        await admin.msb.close();
+
+        // check the result
+        t.ok(resultRemoveWriter, 'Result should not be null');
+        t.is(resultRemoveWriter.pub, writer.wallet.publicKey, 'Result value.pub should match writer public key');
+        t.is(resultRemoveWriter.wk, writer.msb.writingKey, 'Result writing key should match writer writing key');
+        t.is(resultRemoveWriter.isWriter, false, 'Node should not be a writer anymore');
+        t.is(resultRemoveWriter.isIndexer, false, 'Result should not indicate that the peer is an indexer');
+    }
+    catch (error) {
+        t.fail(error.message);
+    }
+
+});
