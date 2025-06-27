8bbf23b899bcdf2afbdc21841f2b57b1072c1af7
leonardostsouza <leonardo.souza@trac.network>
2025-06-27 15:54:15 +0200

    Update wallet dependency

--- msb.mjs
+++ msb.mjs
@@ -1,10 +1,10 @@
 import {MainSettlementBus} from './src/index.js';
 
 const opts = {
-    stores_directory : 'stores3/',
+    stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
-    bootstrap: 'a4951e5f744e2a9ceeb875a7965762481dab0a7bb0531a71568e34bf7abd2c53',
-    channel: '0002tracnetworkmainsettlementbus'
+    bootstrap: '5e049b230bd4dfaaaf57404212920adde3119154988cb464cbda117ce8305baf',
+    channel: 'ls02tracnetworkmainsettlementbus'
 };
 
 const msb = new MainSettlementBus(opts);
