b194fefb1836c3b10f3c711907616552de732257
b-garbacz <bartlomiej.garbacz@trac.network>
2025-07-02 19:52:34 +0200

    add publicKeyToAddress

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -30,17 +30,33 @@ export const safeJsonParse = (str) => {
 // TODO:  write tests for this function
 export const extractPublickeyFromAddress = (address) => {
     if (!b4a.isBuffer(address)) {
-        throw new Error('extractPublicKeyFromAddress: expected a Buffer');
+        throw new Error('extractPublicKeyFromAddress: expected a Buffer.');
     }
 
     if (address.length < 33) {
-        throw new Error('extractPublicKeyFromAddress: address must be at least 33 bytes long');
+        throw new Error('extractPublicKeyFromAddress: address must be at least 33 bytes long.');
     }
 
     const prefix = address.readUInt8(0);
     if (prefix !== TRAC_NETWORK_PREFIX) {
-        throw new Error(`extractPublicKeyFromAddress: invalid network prefix (got 0x${prefix.toString(16)})`);
+        throw new Error(`extractPublicKeyFromAddress: invalid network prefix (got 0x${prefix.toString(16)}).`);
     }
 
     return address.slice(1, 33);
+}
+
+// DO NOT USE IT IN APPLY
+// TODO:  write tests for this function
+export const publicKeyToAddress = (publicKey) => {
+    if (!b4a.isBuffer(address)) {
+        throw new Error('publicKeyToAddress: expected a Buffer.');
+    }
+    if (publicKey.length !== 32) {
+        throw new Error('publicKeyToAddress: publicKey must be 32 bytes long.');
+    }
+
+    const address = b4a.alloc(33);
+    address.writeUInt8(TRAC_NETWORK_PREFIX, 0);
+    publicKey.copy(address, 1);
+    return address;
 }
