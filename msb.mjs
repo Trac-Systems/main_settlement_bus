30dae64666ac6dc6bdebe04933a43c8de4c34543
b-garbacz <bartlomiej.garbacz@trac.network>
2025-07-18 16:01:51 +0200

    revert bootstrap and channel

--- msb.mjs
+++ msb.mjs
@@ -3,6 +3,8 @@ import {MainSettlementBus} from './src/index.js';
 const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
+    bootstrap: 'a4951e5f744e2a9ceeb875a7965762481dab0a7bb0531a71568e34bf7abd2c53',
+    channel: '0002tracnetworkmainsettlementbus',
     enableRoleRequester: true,
     enable_wallet: true,
     enableValidatorObserver: true,
