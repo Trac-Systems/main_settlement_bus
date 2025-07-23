44ef7cbe451617a4a409da34087f77449b210b34
b-garbacz <bartlomiej.garbacz@trac.network>
2025-07-23 15:32:52 +0200

    restore channel

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
     bootstrap: '85154864940d2470e54ad5729c4d33337e6f8bf2778ead87c241b047b6060b69',
-    channel: 'bg0002tracnetworkmainsettlementbus',
+    channel: '0002tracnetworkmainsettlementbus',
     enableRoleRequester: false,
     enable_wallet: true,
     enableValidatorObserver: true,
