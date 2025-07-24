1a68d8360bc91b8e81930d64cc1c4ae2e8827931
b-garbacz <bartlomiej.garbacz@trac.network>
2025-07-24 19:38:28 +0200

    add ValidatorObserverService placeholder

---
+++ src/core/network/services/ValidatorObserverService.js
@@ -0,0 +1,9 @@
+
+class ValidatorObserverService {
+    //TODO PLACEHOLDER: This file is a placeholder for the ValidatorObserverService class.
+    constructor() {
+        // Initialize any necessary properties or services here
+    }    
+}
+
+export default ValidatorObserverService;
