274a8d977d8659acd9769ef3658b19c3ba1a28f9
b-garbacz <bartlomiej.garbacz@trac.network>
2025-07-30 10:43:29 +0200

    uncomment tests

--- test/all.test.js
+++ test/all.test.js
@@ -6,11 +6,11 @@ async function runTests() {
   test.pause();
 
   // await import('./encodings/ApplyOperationEncodings.test.js');
-  // await import('./check/check.test.js');
+  await import('./check/check.test.js');
+  await import('./protobuf/protobuf.test.js');
   //await import('./functions/functions.test.js');
   //await import('./fileUtils/readPublicKeysFromFile.test.js'); 
   //await import('./messageOperations/messageOperations.test.js'); 
-  await import('./protobuf/protobuf.test.js');
   // await import('./apply/apply.test.js'); // after 108-adjust-checkjs-for-incoming-buffer-data commit this test is failing due to changes in check.js
   test.resume();
 }
