eb343328284f0183fc394d0809c7c9d68568ccba
b-garbacz <45511879+b-garbacz@users.noreply.github.com>
2025-07-30 10:44:15 +0200

    131 fix tests for protobuf (#138)
    * Fix tests
    
    * uncomment tests

--- test/fixtures/protobuf.fixtures.js
+++ test/fixtures/protobuf.fixtures.js
@@ -1,91 +1,90 @@
 import b4a from 'b4a';
-import applyOperations from '../../src/utils/protobuf/applyOperations.cjs';
+import { OperationType } from '../../src/utils/constants.js';
+import { addressToBuffer } from '../../src/core/state/utils/address.js';
 
 const validPostTx = {
-  type: applyOperations.OperationType.TX,
-  key: b4a.from('2f0f434b77ac0c85c1c3511ac01e24136d9e5e2425cceca14ef9199f73af5d07', 'hex'),
+  type: OperationType.TX,
+  address: addressToBuffer('trac1c232xtkvyg08zyeurn7l0wrarc4y36fzq5vhcdsgkxe6hdpzuslsm63dw8'),
   txo: {
-    //op: 'post-tx', we need to rid off this from transaction. 
-    tx: b4a.from('2f0f434b77ac0c85c1c3511ac01e24136d9e5e2425cceca14ef9199f73af5d07', 'hex'),
-    is: b4a.from('4dbe14b0ef8cc08a9b513d40f541b3d56194debd03b09e5e571565b0c4196cc6f2b5feb5f763b8599c66bb41e0776bfdd5992657d6547afefecf044899d0b607', 'hex'),
-    w: b4a.from('d9edc49157caa7b0d7508591b1dfba84c3e1a7c1039bf66238f9014c3dd00636', 'hex'),
-    i: b4a.from('3ad0a49ecc5996d72a3ea9a1a57c4fed5bf71ecd0f965ec6225e4deed4fd4d95', 'hex'),
-    ipk: b4a.from('332a5394d68e282f084d6c627febb40d1a5217c8f499a95510f2ba942bc52572', 'hex'),
-    ch: b4a.from('7ceba2e9e4caa40e93b718d689ac543efb095b4d596d36d0abb13bb29c789f94', 'hex'),
-    in: b4a.from('86e08c2a24854e3407b6a85912c088f97556cb0e381afa7912176ad7a2e8346b', 'hex'),
-    bs: b4a.from('df2d5eab173b93fb585ce69463dee7288969869e9a025474aa638f42fab200c5', 'hex'),
-    mbs: b4a.from('70bddc71374515d60a7fbe5abd47227658c4274b2832741404e78601d1d19b6e', 'hex'),
-    ws: b4a.from('a24be863c6498e8b4473f6bb8e702a6d2dcbaaa0c789ba485b3fac0064888fbc7bbaf966e760b03e570c9ce606b1d2961413dec9a54b3f5438c1333d3ec69c0b', 'hex'),
-    wp: b4a.from('3075ff1ab92036d45bf803f1fb0860a73eca2ca1f0c7b3abe4283684b7fcdfd2', 'hex'),
-    wn: b4a.from('50d27298b975f92b605f6bb80c8c0eeb76d2a81bf494470a2b1ef68ffff6d0bc', 'hex')
+    tx: b4a.from('6fb7f6e7f6970477977080f2b46cc837d48605e67691d30bf7511a1417d17ed7', 'hex'),
+    ia: addressToBuffer('trac1lj5ccpygyelu266enxjr2m0hkf2p5nkr4ua2gq9a2pj6ewq54p9qkydxud'),
+    iw: b4a.from('79ef7be837aa9fd8a446a120e1bc1e6bdd99fb5393dc4fa8299d9d5043a7cd98', 'hex'),
+    in: b4a.from('8bcef53a043f42ac7c17344f0c0d56af5b335e412d4042124f27733911169e4f', 'hex'),
+    ch: b4a.from('6ee7b29ce494875c1ea0dc0f9c2997d1aeeb8d21c67809950e145822989c8b2e', 'hex'),
+    is: b4a.from('d8626ea0552bf302921de3536e877796ef131368c9854119660c9c77a4196d4735d60bb87c6a89bbff7d5f8d72a70610d6ee73d62bc5144874cdf23f88e28a05', 'hex'),
+    bs: b4a.from('f68bac445eee3d9276be46aef58328a543e4b7ecc2b5c98c387c1b3ca1a7e85d', 'hex'),
+    mbs: b4a.from('5f3b9a6a516066de365e5e75a7ac0feb55ab7cd4a29facbb028a047fc3f3956e', 'hex'),
+    vs: b4a.from('6ffd5bed879b2edc5ee2108e7ac9c530744dd82775c6369f307dc88aadd07ce5be28fc2fb97b4f793b1bf81d7903bcbe301299ddfb51316d41c70e23d97e4a0d', 'hex'),
+    vn: b4a.from('aa48f5384851cfd58da1b90fb9a444cc4e1bd57659c8b041ad573ebe3c382927', 'hex'),
   }
 };
 
 const validAddIndexer = {
-  type: applyOperations.OperationType.ADD_INDEXER,
-  key: b4a.from('6f0e38b8de086e65c1de22418d8ca54ad00aafe072159e2b05a9305f2611db80', 'hex'),
+  type: OperationType.ADD_INDEXER,
+  address: addressToBuffer('trac1jnafrn8xl3c59s4ml6jusufp2vzm6egkyenrcqvd907j8c9v5j2qnx7wvt'),
   bko: {
-    nonce: b4a.from('59b638b60a07d8ae06aa3e2879bbf5ec7d09cfe566fabc6e866bf853f3458d15', 'hex'),
-    sig: b4a.from('8fb6f2b1a6f0b6aef7c897b7525c2dbd25b41135aa1493f3a517befc932302a2074d19232192d34cca51c380f5ff0b1c5b23464c915e6dce547326db9795610e', 'hex')
+    nonce: b4a.from('a57e57fdda484270d0bed52da04b52ba5bdeb5bc5c0151951d23771e4a8c2e69', 'hex'),
+    sig: b4a.from('65521e25d52ec95c5226f9d4d23604d0805792d8007f0e2f29c1ea3cb4a9725720182267d17463e9d22da073fc0328bd334310ae29505df8ccb3171dc0399d0d', 'hex'),
   }
+
 };
 
 const validRemoveIndexr = {
-  type: applyOperations.OperationType.REMOVE_INDEXER,
-  key: b4a.from('6f0e38b8de086e65c1de22418d8ca54ad00aafe072159e2b05a9305f2611db80', 'hex'),
-  bko: {
-    nonce: b4a.from('ee4bf87667a39a058beadd3a34d5f58f9fa9f2dfef378023a53856bd72bb0225', 'hex'),
-    sig: b4a.from('36fa96d940ea6ac15a8e26af40c160a9fe66cb810fd9837b76b7edac9f0a86d5c928d11f765a0a610b1fe7edcfa965a7f16da3bc2efb63896b7b0810a7dd0e00', 'hex')
-  }
-};
+		type: OperationType.REMOVE_INDEXER,
+		address: addressToBuffer('trac1jnafrn8xl3c59s4ml6jusufp2vzm6egkyenrcqvd907j8c9v5j2qnx7wvt'),
+		bko: {
+			nonce: b4a.from('0c5e531de4cc43145bffbd0758e54d41a41049520d7ced7d40b19e94fa0cc5bc', 'hex'),
+			sig: b4a.from('70283bccce64b03d55840200fe70561cd2f1744fd3a5425c116ebae4b53845c4d510a5e48bcedc4cb7919caf0f77541e694d19a6df8e9419dbb58d840f376d0b', 'hex'),
+		}
+	};
 
 const validAppendWhitelist = {
-  type: applyOperations.OperationType.APPEND_WHITELIST,
-  key: b4a.from('8ddd9cdea5dda7792529fd2b84736bae41e735fd0466c2f7871ee36f2ede8cac', 'hex'),
-  bko: {
-    nonce: b4a.from('aaa649f39261af0b6513a4af4309dc02d6409e01e690dcffb9d2be10986b97ce', 'hex'),
-    sig: b4a.from('ea34436732dabf611eb1eeb9f15b86fd6dce502c256063eee0022a0287fbbb635ed6229ea8bc0ec38fd921ceb6bc7bcf843ed824f59f4d51efbe8861d50adc05', 'hex')
-  }
-};
+		type: OperationType.APPEND_WHITELIST,
+		address: addressToBuffer('trac1c232xtkvyg08zyeurn7l0wrarc4y36fzq5vhcdsgkxe6hdpzuslsm63dw8'),
+		bko: {
+			nonce: b4a.from('775899a44de60dc63b4e8427c72cf0c18cdd9c602aa01300e84f6a9a195771a5', 'hex'),
+			sig: b4a.from('552dcde06d3e7096b91dd254f1ba39bfa38e8821896ee6eabcc70e827a1b95d9378657fa1a6fded98a5a97fffdbc6c2f4eef6caf00d3ad3c4557369aab4fd20b', 'hex'),
+		}
+	};
 
 const validBanValidator = {
-  type: applyOperations.OperationType.BAN_WRITER,
-  key: b4a.from('6f0e38b8de086e65c1de22418d8ca54ad00aafe072159e2b05a9305f2611db80', 'hex'),
-  bko: {
-    nonce: b4a.from('7fb5b38fd0ee409045f249ffb11205649c3c65c9e77db6ed658411095d7db1b1', 'hex'),
-    sig: b4a.from('b41bf967106134c5b748b41ab8607c0a4eb512a0f5127b12ae2d8304d6ee32b0da4ef1a93b6e602a32327f209b3ddc8ef015329f06e171b885b4b65ee69ee307', 'hex')
-  }
-};
+		type: OperationType.BAN_VALIDATOR,
+		address: addressToBuffer('trac1c232xtkvyg08zyeurn7l0wrarc4y36fzq5vhcdsgkxe6hdpzuslsm63dw8'),
+		bko: {
+			nonce: b4a.from('b83421a0d1869a59726a81849f4668c0a7b7042e5917887556f192244d7b3c92', 'hex'),
+			sig: b4a.from('902e4f4d17cf168f4842e2a6dc86058842a4825d2cd698a7e096763d7dbd1cb6c1e8f3f43e4ae99e8594fd5808f6e802cd370b16b330da677b8ec252a96f990d', 'hex'),
+		}
+	};
 
 const validAddAdmin = {
-  type: applyOperations.OperationType.ADD_ADMIN,
-  key: b4a.from('9418310c60fe01bd3788bb1f2fa5be8a2ea7eaa638ce712ca2af835f8eaabe2d', 'hex'),
-  eko: {
-    wk: b4a.from('cb8ddff835b6837422bfe0a8e50ba3b4dca08d4f35667a75681128e2bf4d8ee8', 'hex'),
-    nonce: b4a.from('4ba7252577a07316b4b8dad6a7a91b65e9e6c7fce81a3641120febdc7c455600', 'hex'),
-    sig: b4a.from('2c2fcec55305653421ca3e68097b62615ab8c0ecf2a3eec487e6fe9db2bb662b94b40e6ed39f8e321f88ec550114dd6706aa36b13eda0d3fca06af8234a7980d', 'hex')
-  }
-};
+		type: OperationType.ADD_ADMIN,
+		address: addressToBuffer('trac18qq7h503y3326v6msgvq0jwc0e8jp4t4q53z9p9jvd98arj7mtpqfac04p'),
+		eko: {
+			wk: b4a.from('71c53657a8738b48772f0940398d4f4b01dc56cb32cd2fd84c30359f0cbb08f1', 'hex'),
+			nonce: b4a.from('9027192c6de13b683bc0c0fbcfe09c4e55d47c12c46b122d988f06c282a4be5e', 'hex'),
+			sig: b4a.from('8fb8a3ba30e00c347bca5a8554c47e167f63b248c87e1ea5532eebbad1bc036184fe8872ff65a9e63acfee68d2213a187466c13ff6687d3ab57e5209abd4fb01', 'hex')
+		}
+	};
 
 const validAddWriter = {
-  type: applyOperations.OperationType.ADD_WRITER,
-  key: b4a.from('675cf7ed2813155d92335335dbdb19e9cea3278330185dbb4f441a7fa2207479', 'hex'),
-  eko: {
-    wk: b4a.from('b1210f884cd7c7cd2895a7345388d6ede24e21990e69eb34ae7a60a47c30fdfa', 'hex'),
-    nonce: b4a.from('9387127793640a09af6c11c4e29dc4108fa729230a7c3e587072a09fdf4a555d', 'hex'),
-    sig: b4a.from('82cc5dff1ec55c3eb3ff448a888286b3c97e17279909882a015f8249e8573c8fa64ec57cf9b525a4fca28cca6c56f09376c891e48192a39d8b9a618c69c1ce0c', 'hex')
-  }
-};
+		type: OperationType.ADD_WRITER,
+		address: addressToBuffer('trac1xvqvlzx4w2q2pfqrmycew87kq4rv0q0cewxk68ddvddgk2xm09cqvpc4jc'),
+		eko: {
+			wk: b4a.from('b409a53113949a492b7757370479d2850cac648db4685a1a40d3cdf8da0242e2', 'hex'),
+			nonce: b4a.from('af9698ead07618dd38b4a5bb1bb85de516dc88438f69b5ec8f192ff5a6bcf062', 'hex'),
+			sig: b4a.from('aec41a1266eb6f4992cc097ef6594e49d3c2bc9881173f4570db16e00b2fa7450d79212c059dd15cf3115a1c9771d4c941b126eed41ca44efc517a48a0291609', 'hex')
+		}
+	};
 
 const validRemoveWriter = {
-  type: applyOperations.OperationType.REMOVE_WRITER,
-  key: b4a.from('675cf7ed2813155d92335335dbdb19e9cea3278330185dbb4f441a7fa2207479', 'hex'),
-  eko: {
-    wk: b4a.from('b1210f884cd7c7cd2895a7345388d6ede24e21990e69eb34ae7a60a47c30fdfa', 'hex'),
-    nonce: b4a.from('6c7501bf624767e960bc6a3668a65df3388817f7512d105ef8961b01139509f3', 'hex'),
-    sig: b4a.from('cc0e4e9bc5faf3b54521501d5c69a98edaa244e24df4a847b5dcd1b8167316f674d538812dfa3e296a13eebbc5941a1d717856a298c95b9eee533b141d8f4f07', 'hex')
-  }
-};
+		type: OperationType.REMOVE_WRITER,
+		address: addressToBuffer('trac1xvqvlzx4w2q2pfqrmycew87kq4rv0q0cewxk68ddvddgk2xm09cqvpc4jc'),
+		eko: {
+			wk: b4a.from('b409a53113949a492b7757370479d2850cac648db4685a1a40d3cdf8da0242e2', 'hex'),
+			nonce: b4a.from('c18a02cfa577624b03d34b12dd48daff86c6d5abcb4a0fc11d18775e0bcb88ee', 'hex'),
+			sig: b4a.from('bd9118d8630b01b2cbbd2fb96ffdf4bcb0917e144809a773a6c4bc12dc84ee2afa82d2c5502ae1ff2c829a479f956d0d2e57ad5a501fbb39595c0a6a70bf8408', 'hex')
+		}
+	};
 
 const invalidPayloads = [
   null,
@@ -143,7 +142,7 @@ const invalidPayloads = [
 ];
 
 const invalidPayloadWithMultipleOneOfKeys = {
-  type: applyOperations.OperationType.ADD_WRITER,
+  type: OperationType.ADD_WRITER,
   key: b4a.from('2f0f434b77ac0c85c1c3511ac01e24136d9e5e2425cceca14ef9199f73af5d07', 'hex'),
   bko: { nonce: b4a.from('2f0f434b77ac0c85c1c3511ac01e24136d9e5e2425cceca14ef9199f73af5d07', 'hex') },
   eko: { wk: b4a.from('2f0f434b77ac0c85c1c3511ac01e24136d9e5e2425cceca14ef9199f73af5d07', 'hex') }
