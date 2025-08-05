8762148484a5cd6800907c97894a1d1c1bfa5732
b-garbacz <bartlomiej.garbacz@trac.network>
2025-08-05 19:55:42 +0200

    fix: revert channel

--- msb.mjs
+++ msb.mjs
@@ -4,7 +4,7 @@ const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
     bootstrap: '602d5443c19014e36a01254923afb1df56099d559f282761d70370a0da5d1d8a',
-    channel: 'bg0002tracnetworkmainsettlementbus',
+    channel: '0002tracnetworkmainsettlementbus',
     enable_role_requester: false,
     enable_wallet: true,
     enable_validator_observer: true,
