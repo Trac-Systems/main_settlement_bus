30c8f217275d4ae0834da4be6f02d951f054bab5
leonardostsouza <leonardo.souza@trac.network>
2025-08-05 14:35:39 +0200

    Fix file utils unit tests

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,30 +1,37 @@
 import fs from 'fs';
 import {WHITELIST_FILEPATH} from '../utils/constants.js';
 
-// TODO: The methods in fileUtils need to be refactoed and generalized to improve code reusability
-// -- For example, the readPublicKeysFromFile method is specific to reading a whitelist file set in constants.
-// -- In the future, we might want to generalize this method to read any file and return its content.
-async function readPublicKeysFromFile() {
+
+/**
+ * Reads the contents of the whitelist file and returns an array of strings.
+ * Each line is expected to represent an address. Lines are trimmed and empty lines are ignored.
+ * ATTENTION: The addresses are not checked for validity in this function.
+ *
+ * @param {string} [filepath=WHITELIST_FILEPATH] - The path to the file to read. Defaults to the whitelist file path from constants.
+ * @returns {Promise<string[]>} Resolves with an array of strings (one per valid line).
+ * @throws {Error} If the file does not exist or cannot be read.
+ */
+// TODO: We should generalize this function in the future, so we can improve the reusability of the code.
+async function readPublicKeysFromFile(filepath = WHITELIST_FILEPATH) {
     try {
-        const data = await fs.promises.readFile(WHITELIST_FILEPATH, 'utf8');
-        const pubKeys = data
+        const data = await fs.promises.readFile(filepath, 'utf8');
+        const addresses = data
             .split('\n')
             .map(line => line.trim())
             .filter(line => line.length > 0);
 
-        if (pubKeys.length === 0) {
+        if (addresses.length === 0) {
             console.log('The file does not contain any public keys');
         }
 
-        return pubKeys;
+        return addresses;
     } catch (err) {
         if (err.code === 'ENOENT') {
-            console.log('Whitelist file not found');
+            throw new Error(`Whitelist file not found: ${filepath}`);
         }
-        console.log(`Failed to read public keys from the whitelist file: ${err.message}`);
+        throw new Error(`Failed to read public keys from the whitelist file: ${err.message}`);
     }
 }
-
 
 export default {
     readPublicKeysFromFile
