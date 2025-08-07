ed60aefc272f11efebe89377d5473a243c2ac247
b-garbacz <bartlomiej.garbacz@trac.network>
2025-08-07 16:31:37 +0200

    # This is a combination of 5 commits. # This is the 1st commit message:
    testing commit
    
    # This is the commit message #2:
    
    testing commit2
    
    # This is the commit message #3:
    
    testing commit3
    
    # This is the commit message #4:
    
    testing commit4
    
    # This is the commit message #5:
    
    Fix tests not running on bare environment
    
    testing commit5
    
    testing commit6
    
    testing commit7
    
    testing commit8
    
    testing commit9
    
    last time
    
    comment out not working tests.

--- test/apply/apply.test.js
+++ test/apply/apply.test.js
@@ -1,16 +1,17 @@
-import { default as test } from 'brittle';
+import {default as test} from 'brittle';
 
 async function runCheckTests() {
-  test.pause();
-  await import('./addAdmin.test.js');
-  await import('./addWhitelist.test.js');
-  await import('./addWriter.test.js');
-  await import('./removeWriter.test.js');
-  await import('./addIndexer.test.js');
-  await import('./removeIndexer.test.js');
-  await import('./postTx.test.js');
-  await import('./banValidator.test.js')
-  test.resume();
+    test.pause();
+    await import('./addAdmin/addAdminBasic.test.js');
+    await import('./addAdmin/addAdminRecovery.test.js');
+    await import('./addWhitelist.test.js');
+    await import('./addWriter.test.js');
+    await import('./removeWriter.test.js');
+    await import('./addIndexer.test.js');
+    await import('./removeIndexer.test.js');
+    await import('./postTx.test.js');
+    await import('./banValidator.test.js')
+    test.resume();
 }
 
-runCheckTests();
+await runCheckTests();
