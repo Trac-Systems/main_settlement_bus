179b43f9ba625969cacd16e31f3135c9fb72e89c
Jusuf Suljic <jusuf.uni@gmail.com>
2025-08-19 11:49:33 +0200

    atob hack

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,12 +1,19 @@
+if (typeof globalThis.atob === 'undefined') {
+  globalThis.atob = (str) => Buffer.from(str, 'base64').toString('binary');
+}
+
+if (typeof globalThis.btoa === 'undefined') {
+  globalThis.btoa = (str) => Buffer.from(str, 'binary').toString('base64');
+}
+
 import { blake3 } from '@tracsystems/blake3';
 import b4a from 'b4a';
 
 export async function blake3Hash(input, hashLength = 32) {
   if (typeof input === 'string') {
-    input = b4a.from(input, 'utf8')
+    input = b4a.from(input, 'utf8');
   }
 
   const hashBytes = await blake3(input, hashLength);
-
   return b4a.from(hashBytes);
 }
