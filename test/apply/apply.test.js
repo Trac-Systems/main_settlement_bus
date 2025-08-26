79062ccbeeeb87367f056253b936aefcb915fa95
leonardotc <ltc270486@hotmail.com>
2025-08-26 10:56:20 +0200

    Uncommented the tests

--- test/apply/apply.test.js
+++ test/apply/apply.test.js
@@ -2,15 +2,15 @@ import {default as test} from 'brittle';
 
 async function runCheckTests() {
     test.pause();
-    // await import('./addAdmin/addAdminBasic.test.js');
+    await import('./addAdmin/addAdminBasic.test.js');
     await import('./addAdmin/addAdminRecovery.test.js');
-    // await import('./addWhitelist.test.js');
-    // await import('./addWriter.test.js');
-    // await import('./removeWriter.test.js');
-    // await import('./addIndexer.test.js');
-    // await import('./removeIndexer.test.js');
-    // await import('./postTx.test.js');
-    // await import('./banValidator.test.js')
+    await import('./addWhitelist.test.js');
+    await import('./addWriter.test.js');
+    await import('./removeWriter.test.js');
+    await import('./addIndexer.test.js');
+    await import('./removeIndexer.test.js');
+    await import('./postTx.test.js');
+    await import('./banValidator.test.js')
     test.resume();
 }
 
