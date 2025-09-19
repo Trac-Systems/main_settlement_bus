623d7c14bf5a8cdb3944e31079e80751503b3bf1
Jusuf <61807340+jusufsuljic@users.noreply.github.com>
2025-09-19 16:20:04 +0200

    Exposing endpoint for fetching unconfirmed balance by address (#251)
    * Exposing endpoint for fetching unconfirmed balance by address
    
    * Removed unconfirmed handler and merged the logic in a single /balance router

--- rpc/router.mjs
+++ rpc/router.mjs
@@ -9,7 +9,7 @@ import {
 } from './handlers.mjs';
 
 export const routes = [
-    { method: 'GET', path: '/balance/', handler: handleBalance },
+    { method: 'GET', path: '/balance', handler: handleBalance },
     { method: 'GET', path: '/txv', handler: handleTxv },
     { method: 'GET', path: '/fee', handler: handleFee },
     { method: 'GET', path: '/confirmed-length', handler: handleConfirmedLength },
