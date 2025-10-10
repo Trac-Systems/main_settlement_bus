273056ddc93c88eb1cea36f6058c12dc991be223
Jusuf <61807340+jusufsuljic@users.noreply.github.com>
2025-10-10 10:42:49 +0200

    RPC endpoint for bulk tx payloads fetch implemented (#293)
    * RPC endpoint for bulk tx payloads fetch implemented
    
    * PR fix
    
    * PR merge fix

--- test/acceptance/rpc.test.mjs
+++ test/acceptance/rpc.test.mjs
@@ -121,5 +121,25 @@ describe("API acceptance tests", () => {
                 unsignedLength: expect.any(Number),
             }
         })
-  })
+    })
+
+    // TODO: not sure why but test runner does not work, so this will require more attention.
+    // We can map some of the tx hashes from previous OPs and fetch and assert payload here
+    it("POST /tx-payloads-bulk", async () => {
+        
+        const payload = { hashes: [
+            "test"
+        ]}
+
+        const res = await request(server)
+            .post("/tx-payloads-bulk")
+            .set("Accept", "application/json")
+            .send(JSON.stringify( payload ))
+        
+        expect(res.statusCode).toBe(200)
+        expect(res.body).toMatchObject({
+            results: [],
+            missing:["test"]
+        })
+    })
 })
