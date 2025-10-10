a1c4def1c49c3ed11b3513585e5be14f33d94542
b-garbacz <bartlomiej.garbacz@trac.network>
2025-10-10 17:16:09 +0200

    feat: enable prod rpc

--- rpc/rpc_server.mjs
+++ rpc/rpc_server.mjs
@@ -1,10 +1,10 @@
 import { createServer } from "./create_server.mjs";
 
 // Called by msb.mjs file
-export function startRpcServer(msbInstance, port) {
+export function startRpcServer(msbInstance, host, port) {
     const server = createServer(msbInstance)
 
-    return server.listen(port, () => {
-        console.log(`Running RPC with https at https://localhost:${port}`);
+    return server.listen(port, host, () => {
+        console.log(`Running RPC with https at https://${host}:${port}`);
     });
 }
