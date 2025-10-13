1d95e2e5b2ae161188aa7a53b74131add609d571
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2025-10-13 13:44:00 -0300

    rpc version control

--- rpc/routes/v1.mjs
+++ rpc/routes/v1.mjs
@@ -8,15 +8,15 @@ import {
     handleUnconfirmedLength,
     handleTransactionDetails,
     handleFetchBulkTxPayloads
-} from './handlers.mjs';
+} from '../handlers.mjs';
 
-export const routes = [
+export const v1Routes = [
     { method: 'GET', path: '/balance', handler: handleBalance },
     { method: 'GET', path: '/txv', handler: handleTxv },
     { method: 'GET', path: '/fee', handler: handleFee },
     { method: 'GET', path: '/confirmed-length', handler: handleConfirmedLength },
     { method: 'POST', path: '/broadcast-transaction', handler: handleBroadcastTransaction },
-    { method: 'GET', path: '/tx-hashes/', handler: handleTxHashes },
+    { method: 'GET', path: '/tx-hashes', handler: handleTxHashes },
     { method: 'GET', path: '/unconfirmed-length', handler: handleUnconfirmedLength },
     { method: 'GET', path: '/tx', handler: handleTransactionDetails },
     { method: 'POST', path: '/tx-payloads-bulk', handler: handleFetchBulkTxPayloads },
