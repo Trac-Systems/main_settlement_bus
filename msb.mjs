9fd313b74fb7b57c314ce03610ee8873f0ea9cdf
b-garbacz <bartlomiej.garbacz@trac.network>
2025-10-20 18:06:18 +0200

    chore: Update bootstrap and channel values, and adjust rate limit settings in msb.mjs

--- msb.mjs
+++ msb.mjs
@@ -6,15 +6,15 @@ const args = isPear ? Pear.config.args : process.argv.slice(2);
 const opts = {
     stores_directory : 'stores/',
     store_name : typeof process !== "undefined" ? process.argv[2] : Pear.config.args[0],
-    bootstrap: '602d5443c19014e36a01254923afb1df56099d559f282761d70370a0da5d1d8a',
-    channel: '0002tracnetworkmainsettlementbus',
+    bootstrap: 'd783e5d61aab13bb353c4910dabc08cc92f143c4aa07e156214188a4cca75a2e',
+    channel: 'TESTNET0002tracnetworkmainsettlementbus',
     chain_id: 1,
     enable_role_requester: false,
     enable_auto_transaction_consent: false,
     enable_wallet: true,
     enable_validator_observer: true,
     enable_interactive_mode: true,
-    disable_rate_limit: true,
+    disable_rate_limit: false,
     enable_txlogs: true,
 };
 
@@ -27,7 +27,6 @@ const msb = new MainSettlementBus(args.includes('--rpc') ? rpc_opts : opts);
 
 msb.ready().then(async () => {
     const runRpc = args.includes('--rpc');
-
     if (runRpc) {
         console.log('Starting RPC server...');
         const portIndex = args.indexOf('--port');
@@ -40,7 +39,6 @@ msb.ready().then(async () => {
     } else {
         console.log('RPC server will not be started.');
     }
-
     msb.interactiveMode();
 });
 
