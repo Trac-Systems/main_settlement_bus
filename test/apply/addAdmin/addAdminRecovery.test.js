891d5d2b2bf83007f9150af7372214ad7a251b99
leonardotc <leonardo.cardoso@trac.network>
2025-10-21 13:07:41 +0200

    Fix whitelist apply (#276)
    * Fix apply tests
    
    * Fix postTx tests
    
    * Fix a few on postTx
    
    * Fix post tests
    
    * Fix admin tests
    
    * Stabilized addAIndexer test
    
    * Fix writer tests
    
    * Update
    
    * Wrap up
    
    * Final fixes
    
    * More fixes
    
    * Multiple PR fixes
    
    * Fix acceptance
    
    * Wrap up acceptance fix

--- test/apply/addAdmin/addAdminRecovery.test.js
+++ test/apply/addAdmin/addAdminRecovery.test.js
@@ -1,13 +1,16 @@
-import {test, hook} from 'brittle';
+import {test, hook} from '../../utils/wrapper.js';
 import {
-    initMsbAdmin, initTemporaryDirectory, removeTemporaryDirectory, setupMsbPeer, setupMsbWriter, setupMsbIndexer,
+    initTemporaryDirectory, removeTemporaryDirectory, setupMsbPeer, setupMsbWriter, setupMsbIndexer,
     tryToSyncWriters, waitForAdminEntry, setupMsbAdmin
 } from '../../utils/setupApplyTests.js';
 
 import {randomBytes} from '../../utils/setupApplyTests.js';
 import CompleteStateMessageOperations from '../../../src/messages/completeStateMessages/CompleteStateMessageOperations.js';
+import PartialStateMessageOperations from '../../../src/messages/partialStateMessages/PartialStateMessageOperations.js'
 import {testKeyPair1, testKeyPair2, testKeyPair3, testKeyPair4} from '../../fixtures/apply.fixtures.js';
 import b4a from 'b4a';
+import { decode as decodeAdmin } from '../../../src/core/state/utils/adminEntry.js';
+import { EntryType } from '../../../src/utils/constants.js';
 //TODO: ADD TEST WHEN NON-ADMIN NODE FORGES ADD ADMIN OPERATION AND BROADCASTS IT TO THE STATE -  SHOULD BE REJECTED
 
 let admin, newAdmin;
@@ -28,83 +31,88 @@ hook('Initialize admin for addAdmin tests', async () => {
     admin = await setupMsbAdmin(testKeyPair1, tmpDirectory, baseOptions);
 
     // Setup nodes
-    writer = await setupMsbWriter(admin, 'writer', testKeyPair2, tmpDirectory, baseOptions);
-    indexer1 = await setupMsbWriter(admin, 'indexer1', testKeyPair3, tmpDirectory, baseOptions);
-    indexer2 = await setupMsbWriter(admin, 'indexer2', testKeyPair4, tmpDirectory, baseOptions);
+    writer = await setupMsbWriter(admin, 'writer', testKeyPair2, tmpDirectory, admin.options);
+    indexer1 = await setupMsbWriter(admin, 'indexer1', testKeyPair3, tmpDirectory, admin.options);
+    indexer2 = await setupMsbWriter(admin, 'indexer2', testKeyPair4, tmpDirectory, admin.options);
 
     // Setup indexers after network is stable
     indexer1 = await setupMsbIndexer(indexer1, admin);
     indexer2 = await setupMsbIndexer(indexer2, admin);
 });
 
-
-
 test('Apply function addAdmin for recovery - happy path', async (k) => {
-    try {
-        await tryToSyncWriters(admin, writer, indexer1, indexer2);
+    const formerBootstrap = admin.options.bootstrap
+    await tryToSyncWriters(admin, writer, indexer1, indexer2);
 
-        const adminEntryBefore = await admin.msb.state.getAdminEntry();
-        const admI1 = await indexer1.msb.state.getAdminEntry();
-        const admI2 = await indexer2.msb.state.getAdminEntry();
-        const admW = await writer.msb.state.getAdminEntry();
+    const adminEntryBefore = await admin.msb.state.getAdminEntry();
+    const admI1 = await indexer1.msb.state.getAdminEntry();
+    const admI2 = await indexer2.msb.state.getAdminEntry();
+    const admW = await writer.msb.state.getAdminEntry();
 
-        await waitForAdminEntry(admin, {
-            address: admin.wallet.address,
-            wk: admin.msb.state.writingKey
-        })
+    await waitForAdminEntry(admin, {
+        address: admin.wallet.address,
+        wk: admin.msb.state.writingKey
+    })
 
-        await waitForAdminEntry(indexer1, {
-            address: admin.wallet.address,
-            wk: admin.msb.state.writingKey
-        })
+    await waitForAdminEntry(indexer1, {
+        address: admin.wallet.address,
+        wk: admin.msb.state.writingKey
+    })
 
-        await waitForAdminEntry(indexer2, {
-            address: admin.wallet.address,
-            wk: admin.msb.state.writingKey
-        })
+    await waitForAdminEntry(indexer2, {
+        address: admin.wallet.address,
+        wk: admin.msb.state.writingKey
+    })
 
-        await waitForAdminEntry(writer, {
-            address: admin.wallet.address,
-            wk: admin.msb.state.writingKey
-        })
+    await waitForAdminEntry(writer, {
+        address: admin.wallet.address,
+        wk: admin.msb.state.writingKey
+    })
 
-        k.ok(adminEntryBefore !== null, 'Admin entry should not be null before recovery');
-        k.ok(b4a.equals(adminEntryBefore.wk, admin.options.bootstrap), 'Admin writing key in base should match bootstrap key');
-        k.ok(b4a.equals(adminEntryBefore.wk, admI1.wk), 'Admin entry writer key the same as indexer');
-        k.ok(b4a.equals(admI1.wk, admI2.wk), 'Admin entry should be the same for both indexers');
-        k.ok(b4a.equals(admI1.wk, admW.wk), 'Admin entry should be the same for writer');
-        k.ok(adminEntryBefore.address === admI1.address, 'Admin entry address the same as indexer');
-        k.ok(admI1.address === admI2.address, 'Admin address should be the same for both indexers');
-        k.ok(admI1.address === admW.address, 'Admin address should be the same for writer');
-        const adminAddressBeforeRecovery = admin.wallet.address
-        await admin.msb.close(); // close the admin instance to simulate recovery
+    // waitForIndexersConnection
 
-        // Simulate recovery by creating a new admin instance
-        newAdmin = await setupMsbPeer('newAdmin', testKeyPair1, tmpDirectory, admin.options);
-        await newAdmin.msb.ready();
-        const addAdminMessage = await CompleteStateMessageOperations.assembleAddAdminMessage(
-            newAdmin.wallet,
-            newAdmin.msb.state.writingKey
-        );
-        await writer.msb.state.append(addAdminMessage); // Send `add admin` request to apply function
-        await tryToSyncWriters(newAdmin, writer, indexer1, indexer2);
-        await waitForAdminEntry(newAdmin, {
-            address: newAdmin.wallet.address,
-            wk: newAdmin.msb.state.writingKey
-        })
+    k.ok(adminEntryBefore !== null, 'Admin entry should not be null before recovery');
+    k.ok(b4a.equals(adminEntryBefore.wk, admin.options.bootstrap), 'Admin writing key in base should match bootstrap key');
+    k.ok(b4a.equals(adminEntryBefore.wk, admI1.wk), 'Admin entry writer key the same as indexer');
+    k.ok(b4a.equals(admI1.wk, admI2.wk), 'Admin entry should be the same for both indexers');
+    k.ok(b4a.equals(admI1.wk, admW.wk), 'Admin entry should be the same for writer');
+    k.ok(adminEntryBefore.address === admI1.address, 'Admin entry address the same as indexer');
+    k.ok(admI1.address === admI2.address, 'Admin address should be the same for both indexers');
+    k.ok(admI1.address === admW.address, 'Admin address should be the same for writer');
+    const adminAddressBeforeRecovery = admin.wallet.address
+    
+    // Simulate recovery by creating a new admin instance
+    newAdmin = await setupMsbPeer('newAdmin', testKeyPair1, tmpDirectory, admin.options);
+    await admin.msb.close(); // close the admin instance to simulate recovery
+    await newAdmin.msb.ready();
+    await newAdmin.msb.state.append(null);
+    const validity = b4a.toString(await newAdmin.msb.state.getIndexerSequenceState(), 'hex')
+    const addAdminMessage = await PartialStateMessageOperations.assembleAdminRecoveryMessage(
+        newAdmin.wallet,
+        b4a.toString(newAdmin.msb.state.writingKey, 'hex'),
+        validity
+    );
 
-        const adminEntryAfter = await newAdmin.msb.state.getAdminEntry(); // check if the admin entry was added successfully in the base
-        k.ok(adminEntryAfter, 'Result should not be null');
-        k.ok(adminEntryAfter.address === newAdmin.wallet.address, 'New Admin address in base should match new admin wallet address');
-        k.ok(adminAddressBeforeRecovery === newAdmin.wallet.address, 'New Admin wallet address should be the same as old admin wallet address');
-        k.ok(b4a.equals(adminEntryAfter.wk, newAdmin.msb.state.writingKey), 'New Admin writing key in base should match new admin MSB writing key');
-        k.ok(!b4a.equals(adminEntryBefore.wk, adminEntryAfter.wk), 'New Admin writing key in base should have changed');
-        k.ok(!b4a.equals(adminEntryAfter.wk, newAdmin.options.bootstrap), 'New Admin should not be bootstrap anymore');
-        //k.ok(newAdmin.msb.state.isWritable(), 'New Admin should be a writer');
-        // k.ok(newAdmin.msb.state.isIndexer(), 'New Admin should be an indexer'); // wait until holepunch team will fix bug with rotation.
-    } catch (error) {
-        k.fail(error.message);
-    }
+    const rawTx = await CompleteStateMessageOperations.assembleAdminRecoveryMessage(
+        writer.wallet,
+        addAdminMessage.address,
+        b4a.from(addAdminMessage.rao.tx, 'hex'),
+        b4a.from(addAdminMessage.rao.txv, 'hex'),
+        b4a.from(addAdminMessage.rao.iw, 'hex'),
+        b4a.from(addAdminMessage.rao.in, 'hex'),
+        b4a.from(addAdminMessage.rao.is, 'hex')
+    )
+    await writer.msb.state.append(rawTx)
+    await tryToSyncWriters(writer, indexer1, indexer2, newAdmin);
+    const adminEntryAfter = decodeAdmin(await writer.msb.state.get(EntryType.ADMIN)); // check if the admin entry was added successfully in the base
+    k.ok(adminEntryAfter, 'Result should not be null');
+    k.ok(adminEntryAfter.address === newAdmin.wallet.address, 'New Admin address in base should match new admin wallet address');
+    k.ok(adminAddressBeforeRecovery === newAdmin.wallet.address, 'New Admin wallet address should be the same as old admin wallet address');
+    k.ok(b4a.equals(adminEntryAfter.wk, newAdmin.msb.state.writingKey), 'New Admin writing key in base should match new admin MSB writing key');
+    k.ok(!b4a.equals(adminEntryBefore.wk, adminEntryAfter.wk), 'New Admin writing key in base should have changed');
+    k.ok(!b4a.equals(adminEntryAfter.wk, formerBootstrap), 'New Admin should not be bootstrap anymore');
+    //k.ok(newAdmin.msb.state.isWritable(), 'New Admin should be a writer');
+    // k.ok(newAdmin.msb.state.isIndexer(), 'New Admin should be an indexer'); // wait until holepunch team will fix bug with rotation.
 });
 
 hook('Clean up addAdmin recovery setup', async () => {
---
+++ test/apply/postTx/postTx.test.js
@@ -0,0 +1,223 @@
+import {hook, test} from '../../utils/wrapper.js';
+import b4a from "b4a";
+import {
+    generatePostTx,
+    initTemporaryDirectory,
+    randomBytes,
+    removeTemporaryDirectory,
+    setupMsbAdmin,
+    setupMsbPeer,
+    setupMsbWriter,
+    tick,
+    fundPeer,
+    deployExternalBootstrap,
+    tryToSyncWriters,
+    waitForHash,
+    waitDemotion,
+    promoteToWriter
+} from '../../utils/setupApplyTests.js';
+import {safeDecodeApplyOperation, safeEncodeApplyOperation} from '../../../src/utils/protobuf/operationHelpers.js'
+import {testKeyPair1, testKeyPair2, testKeyPair3, testKeyPair4, testKeyPair5} from '../../fixtures/apply.fixtures.js';
+import {OperationType} from "../../../src/utils/constants.js";
+import {addressToBuffer} from "../../../src/core/state/utils/address.js";
+import { $TNK } from '../../../src/core/state/utils/balance.js';
+import CompleteStateMessageOperations from '../../../src/messages/completeStateMessages/CompleteStateMessageOperations.js';
+
+let tmpDirectory, admin, writer, externalNode, externalBootstrap, maliciousPeer;
+
+const close = async node => {
+    await node.msb.close()
+}
+
+hook('Initialize nodes', async t => {
+    const randomChannel = randomBytes(32).toString('hex');
+
+    const baseOptions = {
+        enable_txlogs: false,
+        enable_interactive_mode: false,
+        enable_role_requester: false,
+        enable_validator_observer: false,
+        channel: randomChannel,
+    }
+
+    tmpDirectory = await initTemporaryDirectory();
+    admin = await setupMsbAdmin(testKeyPair1, tmpDirectory, baseOptions);
+    writer = await setupMsbWriter(admin, 'writer1', testKeyPair2, tmpDirectory, admin.options);
+    externalNode = await setupMsbPeer('reader1', testKeyPair4, tmpDirectory, admin.options);
+    await fundPeer(admin, externalNode, $TNK(1n)) // we fund it since it will deploy stuff
+    externalBootstrap = await deployExternalBootstrap(writer, externalNode)
+    await tryToSyncWriters(writer, admin)
+    maliciousPeer = await setupMsbPeer('maliciousWriter', testKeyPair5, tmpDirectory, admin.options);
+    await fundPeer(admin, maliciousPeer, $TNK(10n))
+});
+
+test('handleApplyTxOperation (apply) - Append POST_TX into the base - Happy path', async t => {
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    await writer.msb.state.append(postTx)
+    await waitForHash(writer, txHash)
+    await tick();
+
+    const result = await writer.msb.state.get(txHash);
+    t.ok(result, 'post tx added to the base');
+})
+
+// Negative cases
+
+// TODO: This test has been disabled because we got rid of JSON schema validation in favor of protobuf validation. To enable this test again, we need to write a fake protobuf schema for postTx (maybe also for other operations) and forge an invalid payload.
+
+// test('nested object in postTx', async t => {
+//     //is already tested in /test/check/postTx.test.js
+//     let { postTx, preTxHash: txHash } = await generatePostTx(admin.msb, admin.wallet, legitWallet)
+//     postTx = {
+//         ...postTx,
+//         foo: 'bar',
+//     }
+//     await admin.msb.state.append(postTx);
+//     await tick();
+
+//     t.absent(await admin.msb.state.get(txHash), 'post tx with nested object should not be added to the base');
+//     postTx = {
+//         ...postTx,
+//         value: {
+//             ...postTx.value,
+//             foo: 'bar',
+//         }
+//     }
+//     await admin.msb.state.append(postTx);
+//     await tick();
+//     t.absent(await admin.msb.state.get(txHash), 'post tx with nested object in value property should not be added to the base');
+
+// })
+
+test('handleApplyTxOperation (apply) - different operation type', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    const replaceByte = (input, index, replacement) => {
+        const bufferHex = b4a.from(input, 'hex');
+        bufferHex[index] = replacement;
+        return b4a.from(bufferHex, 'hex');
+    }
+
+    const replacedPostTx = replaceByte(b4a.toString(postTx, 'hex'), 1, OperationType.ADD_ADMIN);
+
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(replacedPostTx);
+    })
+    await tick();
+
+    t.absent(await writer.msb.state.get(txHash), 'post tx with incorrect operation type should not be added to the base');
+})
+
+// test('handleApplyTxOperation (apply) - replay attack', async t => {
+//     const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+//     const rawTx = await CompleteStateMessageOperations.assembleCompleteTransactionOperationMessage(
+//         writer.msb.wallet,
+//         postTx.address,
+//         b4a.from(postTx.txo.tx, 'hex'),
+//         b4a.from(postTx.txo.txv, 'hex'),
+//         b4a.from(postTx.txo.iw, 'hex'),
+//         b4a.from(postTx.txo.in, 'hex'),
+//         b4a.from(postTx.txo.ch, 'hex'),
+//         b4a.from(postTx.txo.is, 'hex'),
+//         b4a.from(postTx.txo.bs, 'hex'),
+//         b4a.from(postTx.txo.mbs, 'hex')
+//     );
+//     await writer.msb.state.append(rawTx);
+//     await tick();
+//     await sleep(500)
+//     const firstRes = await writer.msb.state.base.view.get(txHash);
+
+//     await writer.msb.state.append(rawTx);
+//     await sleep(500)
+//     await tick();
+//     const secondRes = await writer.msb.state.base.view.get(txHash);
+
+//     t.is(firstRes.seq, secondRes.seq, 'post tx should not be added to the base twice');
+// })
+
+test('handleApplyTxOperation (apply) - invalid postTx signature (adversary signature - writer signature)', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    let decodedPostTx = safeDecodeApplyOperation(postTx);
+
+    decodedPostTx.txo.vs = maliciousWriter.wallet.sign(
+        b4a.concat([decodedPostTx.txo.tx, decodedPostTx.txo.vn])
+    );
+    const encodedMaliciousPostTx = safeEncodeApplyOperation(decodedPostTx);
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    })
+
+    const result = await admin.msb.state.get(txHash);
+    t.absent(result, 'adversary\'s fake signature should not be added to the base');
+});
+
+
+test('handleApplyTxOperation (apply) - invalid postTx signature (adversary signature - peer signature)', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+
+    let decodedPostTx = safeDecodeApplyOperation(postTx);
+
+    decodedPostTx.txo.is = maliciousWriter.wallet.sign(
+        decodedPostTx.txo.tx
+    );
+    const encodedMaliciousPostTx = safeEncodeApplyOperation(decodedPostTx);
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    })
+
+    const result = await writer.msb.state.get(txHash);
+    t.absent(result, 'adversary prepared fake postTx signature (third key pair) should not be added to the base');
+});
+
+test('handleApplyTxOperation (apply) - oversized transaction', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    let decodedPostTx = safeDecodeApplyOperation(postTx);
+
+    decodedPostTx.txo.vn = randomBytes(5000);
+    const encodedMaliciousPostTx = safeEncodeApplyOperation(decodedPostTx);
+
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    })
+    const result = await admin.msb.state.get(txHash);
+    t.absent(result, 'oversized post tx should not be added to the base');
+});
+
+test('handleApplyTxOperation (apply) - invalid postTx address (malicious node replaces address)', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    let decodedPostTx = safeDecodeApplyOperation(postTx);
+    decodedPostTx.address = addressToBuffer(maliciousWriter.wallet.address);
+    const encodedMaliciousPostTx = safeEncodeApplyOperation(decodedPostTx);
+    await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    })
+    const result = await admin.msb.state.get(txHash);
+    t.absent(result, 'post tx with malicious address should not be added to the base');
+});
+
+test('handleApplyTxOperation (apply) - invalid postTx txo.ia (malicious node replaces ia)', async t => {
+    const maliciousWriter = await promoteToWriter(admin, maliciousPeer)
+    const {postTx, txHash} = await generatePostTx(writer, externalNode, externalBootstrap)
+    let decodedPostTx = safeDecodeApplyOperation(postTx);
+    decodedPostTx.txo.ia = addressToBuffer(maliciousWriter.wallet.address);
+    const encodedMaliciousPostTx = safeEncodeApplyOperation(decodedPostTx);
+    await waitDemotion(maliciousWriter, async () => {
+        await maliciousWriter.msb.state.append(encodedMaliciousPostTx);
+    })
+    const result = await admin.msb.state.get(txHash);
+    t.absent(result, 'post tx with malicious txo.ia should not be added to the base');
+});
+
+hook('Clean up postTx setup', async t => {
+    // close msbBoostrap and remove temp directory
+    if (writer?.msb) await close(writer)
+    if (externalNode?.msb) await close(externalNode)
+    if (admin?.msb) await close(admin)
+    if (maliciousPeer?.msb) await close(maliciousPeer)
+    if (tmpDirectory) await removeTemporaryDirectory(tmpDirectory);
+});
