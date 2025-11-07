0a466ae442086a716ee3c0a78b2d5d6cbef45755
b-garbacz <bartlomiej.garbacz@trac.network>
2025-11-07 11:56:48 +0100

    bump version to 0.2.0 in package.json and package-lock.json; remove unused test code from index.js

--- src/index.js
+++ src/index.js
@@ -974,26 +974,6 @@ export class MainSettlementBus extends ReadyResource {
                     this.#state.writingKey,
                 );
                 break;
-
-            // DELETE BEFORE DEPLOYMENT /TEST
-            case '/test':
-                const contentHash = randomBytes(32).toString('hex');
-                const randomExternalBootstrap = "5adb970a73e20e8e2e896cd0c30cf025a0b32ec6fe026b98c6714115239607ac"
-                const randomWk = randomBytes(32).toString('hex');
-                const txValidity = await this.#state.getIndexerSequenceState();
-                const msbBootstrap = this.bootstrap.toString('hex');
-                const assembledTransactionOperation = await PartialStateMessageOperations.assembleTransactionOperationMessage(
-                    this.#wallet,
-                    randomWk,
-                    txValidity.toString('hex'),
-                    contentHash,
-                    randomExternalBootstrap,
-                    msbBootstrap
-                )
-                await this.broadcastPartialTransaction(assembledTransactionOperation);
-
-                break;
-            case '/test2':
                 let messageCount = 0;
                 const duration = 60000;
                 const interval = setInterval(async () => {
