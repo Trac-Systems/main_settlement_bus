833aaffef854ad2ff537fd542741b9aec55a0e05
b-garbacz <bartlomiej.garbacz@trac.network>
2025-12-03 18:20:10 +0100

    feat(test): add tests to validate confirmed length endpoint response

--- tests/acceptance/v1/confirmed-length/confirmed-length.test.mjs
+++ tests/acceptance/v1/confirmed-length/confirmed-length.test.mjs
@@ -7,5 +7,13 @@ export const registerConfirmedLengthTests = (context) => {
             expect(res.statusCode).toBe(200)
             expect(res.body).toEqual({ confirmed_length: expect.any(Number) })
         })
+
+        it("matches signed length from state", async () => {
+            const res = await request(context.server).get("/v1/confirmed-length")
+            expect(res.statusCode).toBe(200)
+
+            const signedLength = context.rpcMsb.state.getSignedLength()
+            expect(res.body).toEqual({ confirmed_length: signedLength })
+        })
     })
 }
