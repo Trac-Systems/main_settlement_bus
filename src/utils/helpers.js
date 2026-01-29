537d2372eb056408518edebab10754f6f5c1fdf5
b-garbacz <bartlomiej.garbacz@trac.network>
2026-01-29 18:25:14 +0100

    feat: Implement generateUUID

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 import b4a from "b4a";
 import {bufferToAddress} from "../core/state/utils/address.js";
 import { EntryType } from "./constants.js";
-
+import { v7 as uuidv7 } from 'uuid';
 //TODO: change file name or split functions below into multiple files (Remember to update imports and tests accordingly)
 
 export function isHexString(string) {
@@ -94,4 +94,8 @@ export function isTransactionRecordPut(entry) {
     const isHex = isHexString(entry.key);
     const is64 = entry.key.length === 64;
     return isPut && isHex && is64;
+}
+
+export function generateUUID() {
+    return uuidv7();
 }
