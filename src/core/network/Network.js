53b98eab443a7e707fd18470a667eee3ba46642d
b-garbacz <bartlomiej.garbacz@trac.network>
2026-02-23 14:35:32 +0100

    feat: integrate TransactionCommitService into Network class for improved transaction handling

--- src/core/network/Network.js
+++ src/core/network/Network.js
@@ -21,6 +21,7 @@ import NetworkWalletFactory from './identity/NetworkWalletFactory.js';
 import { networkMessageFactory } from '../../messages/network/v1/networkMessageFactory.js';
 import TransactionRateLimiterService from './services/TransactionRateLimiterService.js';
 import PendingRequestService from './services/PendingRequestService.js';
+import TransactionCommitService from "./services/TransactionCommitService.js";
 
 // -- Debug Mode --
 // TODO: Implement a better debug system in the future. This is just temporary.
@@ -47,6 +48,7 @@ class Network extends ReadyResource {
     #maxPendingConnections;
     #rateLimiter;
     #pendingRequestsService;
+    #transactionCommitService;
     #wallet;
 
     /**
@@ -60,7 +62,8 @@ class Network extends ReadyResource {
         this.#connectTimeoutMs = config.connectTimeoutMs || 5000;
         this.#maxPendingConnections = config.maxPendingConnections || 50;
         this.#pendingConnections = new Map();
-        this.#transactionPoolService = new TransactionPoolService(state, address, this.#config);
+        this.#transactionCommitService = new TransactionCommitService(this.#config);
+        this.#transactionPoolService = new TransactionPoolService(state, address, this.#transactionCommitService ,this.#config);
         this.#validatorObserverService = new ValidatorObserverService(this, state, address, this.#config);
         this.#validatorConnectionManager = new ConnectionManager(this.#config);
         this.#validatorMessageOrchestrator = new MessageOrchestrator(this.#validatorConnectionManager, state, this.#config);
@@ -106,6 +109,7 @@ class Network extends ReadyResource {
         this.cleanupNetworkListeners();
         this.cleanupPendingConnections();
         this.#pendingRequestsService.close();
+        this.#transactionCommitService.close();
 
         if (this.#swarm !== null) {
             this.#swarm.destroy();
@@ -200,6 +204,7 @@ class Network extends ReadyResource {
                 this.#rateLimiter,
                 this.#transactionPoolService,
                 this.#pendingRequestsService,
+                this.#transactionCommitService,
                 this.#config
             );
 
@@ -295,11 +300,6 @@ class Network extends ReadyResource {
                 await this.#finalizeConnection(publicKey, type, connection);
             }
         }
-    }
-
-    async isConnected(publicKey) {
-        return this.#swarm.peers.has(publicKey) &&
-            this.#swarm.peers.get(publicKey).connectedTime != -1
     }
 
     async #finalizeConnection(publicKey, type, connection) {
