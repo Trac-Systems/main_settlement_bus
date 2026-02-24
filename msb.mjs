b847ca8d572c70bce60d86123c07726a944decfb
leonardotc <ltc270486@hotmail.com>
2026-02-24 14:07:23 +0100

    Use flags on package.json

--- msb.mjs
+++ msb.mjs
@@ -1,31 +1,11 @@
 import { MainSettlementBus } from './src/index.js';
 import { startRpcServer } from './rpc/rpc_server.js';
-import { createConfig } from './src/config/env.js';
-import { getArguments, resolveEnvironment } from './src/config/args.js';
+import { isRpcEnabled, resolveConfig } from './src/config/args.js';
 
-const args = getArguments();
-const runRpc = args.includes('--rpc');
-const selectedEnv = resolveEnvironment(args);
-const storeName = args[0]
-const hostIndex = args.indexOf('--host');
-const host = (hostIndex !== -1 && args[hostIndex + 1]) ? args[hostIndex + 1] : undefined;
-const portIndex = args.indexOf('--port');
-const port = (portIndex !== -1 && args[portIndex + 1]) ? parseInt(args[portIndex + 1], 10) : undefined;
-
-const rpc = {
-    storeName,
-    enableWallet: false,
-    enableInteractiveMode: false,
-    host,
-    port
-}
-
-const options = runRpc ? rpc : { storeName }
-const config = createConfig(selectedEnv, options)
-const msb = new MainSettlementBus(config);
+const msb = new MainSettlementBus(resolveConfig());
 
 msb.ready().then(async () => {
-    if (runRpc) {
+    if (isRpcEnabled()) {
         console.log('Starting RPC server...');
         startRpcServer(msb, config);
     } else {
