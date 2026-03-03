6b076e9e9bc01adf7de69c2a0f069633b0c18bdd
leonardostsouza <leonardo.souza@trac.network>
2026-03-03 09:18:05 +0100

    test: organize v1 tests in a separate module

--- tests/unit/network/networkModule.test.js
+++ tests/unit/network/networkModule.test.js
@@ -4,16 +4,8 @@ async function runNetworkModuleTests() {
     test.pause();
     await import('./NetworkWalletFactory.test.js');
     await import('./ProtocolSession.test.js');
-    await import('./v1/v1.handlers.test.js');
     await import('./services/services.test.js');
-    await import('./services/ValidatorHealthCheckService.test.js');
-    await import('./services/TransactionRateLimiterService.test.js');
-    await import('./v1/V1ResultCode.test.js');
-    await import('./v1/V1BroadcastTransactionResponse.test.js');
-    await import('./v1/V1ValidationSchema.test.js');
-    await import('./v1/V1ValidationErrorMapper.test.js');
-    await import('./v1/V1BroadcastTransactionOperationHandler.test.js');
-    await import('./v1/NetworkMessageRouterV1.test.js');
+    await import('./v1/v1.test.js');
     test.resume();
 }
 
