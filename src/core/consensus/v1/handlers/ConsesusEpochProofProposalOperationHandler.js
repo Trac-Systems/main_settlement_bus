764905c21e4747ecd9204df379a6a19e2948412a
leonardostsouza <leonardo.souza@trac.network>
2026-06-30 17:24:29 +0200

    feat: implement emit event placeholders on proof proposal op handlers

--- src/core/consensus/v1/handlers/ConsesusEpochProofProposalOperationHandler.js
+++ src/core/consensus/v1/handlers/ConsesusEpochProofProposalOperationHandler.js
@@ -1,31 +1,34 @@
 import V1EpochProofProposalRequest from "../validators/V1EpochProofProposalRequest.js";
 import V1EpochProofProposalApproval from "../validators/V1EpochProofProposalApproval.js";
-import {networkMessageFactory} from "../../../../messages/network/v1/networkMessageFactory.js";
+import { networkMessageFactory } from "../../../../messages/network/v1/networkMessageFactory.js";
 import b4a from "b4a";
 import { V1ProtocolError } from "../../../network/protocols/v1/V1ProtocolError.js";
-import {getResultCode, V1ConsensusProtocolError} from "../V1ConsensusProtocolError.js"
-import {ConsensusResultCode, NETWORK_CAPABILITIES, ResultCode} from "../../../../utils/constants.js";
+import { getResultCode, V1ConsensusProtocolError } from "../V1ConsensusProtocolError.js"
+import { ConsensusResultCode, NETWORK_CAPABILITIES, ResultCode } from "../../../../utils/constants.js";
 import { consensusMessageFactory } from "../../../../messages/consensus/v1/consensusMessageFactory.js";
 import { bufferToAddress } from "../../../state/utils/address.js";
 import { verifyWesolowski } from "@tracsystems/trac-vdf";
-import {publicKeyToAddress} from "../../../../utils/helpers.js";
+import { publicKeyToAddress } from "../../../../utils/helpers.js";
 import ConnectionOperationHandler from "../../../network/protocols/shared/ConnectionOperationHandler.js";
+import ConsensusEpochProofProposalEventHandler from "./ConsensusEpochProofProposalEventHandler.js";
 
 // Minion interface to verify & sign proposals
 
 // Responsibilities:
 // Validate -> close connections -> emit events to notify system-> send responses 
 
-class ConsensusEpochProofProposalOperationHandler extends ConnectionOperationHandler{
+class ConsensusEpochProofProposalOperationHandler extends ConnectionOperationHandler {
     #proofProposalRequestValidator;
     #proofProposalApprovalValidator;
     #wallet;
     #state;
+    #eventHandler;
 
     constructor(state, wallet, config) {
         super(config)
         this.#state = state;
         this.#wallet = wallet;
+        this.#eventHandler = new ConsensusEpochProofProposalEventHandler(config);
         this.#proofProposalRequestValidator = new V1EpochProofProposalRequest(config);
         this.#proofProposalApprovalValidator = new V1EpochProofProposalApproval(config);
     }
@@ -38,16 +41,31 @@ class ConsensusEpochProofProposalOperationHandler extends ConnectionOperationHan
      * @throws {V1ProtocolError|Error} If request validation or response building fails.
      */
     async handleRequest(message, connection) {
-        // TODO:  -- emit event "epoch_proposal_received"
+        const eventContext = this.#buildRequestEventContext(message, connection);
+        await this.#eventHandler.onEpochProposalReceived(eventContext);
+
         let resultCode = ConsensusResultCode.OK;
+        let validationError;
+        let proofProposal;
         try {
             await this.#proofProposalRequestValidator.validate(message, connection);
-            //TODO:  -- emit event "epoch_proposal_validation_success"
+            proofProposal = message.proof_proposal;
+            await this.#eventHandler.onEpochProposalValidationSuccess({
+                ...eventContext,
+                resultCode,
+                proofProposal
+            });
         } catch (e) {
+            validationError = e;
             resultCode = getResultCode(e);
-            //TODO: -- emit event "epoch_proposal_validation_failure"
-            // TODO: Add condition if INVALID_ADDRESS_ASSERTION then blacklist specific remogePublicKey
-        } finally {
+            // TODO: In the event handler, add condition if INVALID_ADDRESS_ASSERTION, then blacklist specific remote address/pubKey
+            await this.#eventHandler.onEpochProposalValidationFailure({
+                ...eventContext,
+                resultCode,
+                error: validationError
+            });
+        }
+        finally {
             await this.#sendEpochProofProposalApprovalResponse(message.session_id, connection, message.proof_proposal, resultCode);
         }
     }
@@ -57,22 +75,67 @@ class ConsensusEpochProofProposalOperationHandler extends ConnectionOperationHan
      *
      * @param {object} message Decoded consensus v1 message containing `proof_proposal_response`.
      * @param {object} connection Peer connection context used by the response validator.
-     * @param proofProposal
-     * @returns {Promise<object>} Validated proof proposal approval.
-     * @throws {V1ProtocolError|Error} If response validation fails.
+     * @param {object} proofProposal Original proof proposal used by the response validator.
+     * @returns {Promise<{resultCode: number, approval?: object}>} Approval handling outcome.
      */
 
     async handleApproval(message, connection, proofProposal) {
-        // TODO:  emit event "received_response" (maybe not necessary)
+        const eventContext = this.#buildApprovalEventContext(message, connection, proofProposal);
+        await this.#eventHandler.onApprovalResponseReceived(eventContext); // NOTE: Maybe not needed. Investigate. For now, this will be only a placeholder
+
         let resultCode = ConsensusResultCode.OK;
+        let approval;
         try {
             await this.#proofProposalApprovalValidator.validate(message, connection, proofProposal);
-            const approval = message.proof_proposal_response.approval;
-            // TODO:  emit event "response_success" , include  result code and approval
+            approval = message.proof_proposal_response.approval;
         } catch (e) {
             resultCode = getResultCode(e);
-            // TODO:  emit event "response_failure" include result code without approval because this is invalid
+            await this.#eventHandler.onApprovalResponseFailure({
+                ...eventContext,
+                resultCode,
+                error: e
+            });
+            return { resultCode };
         }
+
+        await this.#eventHandler.onApprovalResponseSuccess({
+            ...eventContext,
+            resultCode,
+            approval
+        });
+        return { resultCode, approval };
+    }
+
+    #buildRequestEventContext(message, connection) {
+        const remotePublicKey = connection?.remotePublicKey; // NOTE: Maybe should use address instead
+
+        return {
+            message,
+            connection,
+            sessionId: message?.session_id,
+            remotePublicKey,
+            remotePublicKeyHex: this.#toRemotePublicKeyHex(remotePublicKey)
+        };
+    }
+
+    // TODO: This function is mostly copy-past from the one above. Refactor
+    #buildApprovalEventContext(message, connection, proofProposal) {
+        const remotePublicKey = connection?.remotePublicKey; // NOTE: Maybe should use address instead
+
+        return {
+            message,
+            connection,
+            sessionId: message?.session_id,
+            remotePublicKey,
+            remotePublicKeyHex: this.#toRemotePublicKeyHex(remotePublicKey),
+            proofProposal
+        };
+    }
+
+    #toRemotePublicKeyHex(remotePublicKey) {
+        if (b4a.isBuffer(remotePublicKey)) return b4a.toString(remotePublicKey, 'hex');
+        if (typeof remotePublicKey === 'string') return remotePublicKey.toLowerCase();
+        return undefined;
     }
 
     async #buildProofProposalApproval(sessionId, proofProposal, resultCode) {
