df494160084c664edc04d2c61519c98373b5cb17
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2026-08-04 10:20:02 -0300

    fix: return bootstrap

--- src/config/env.js
+++ src/config/env.js
@@ -126,7 +126,7 @@ const configData = {
         addressPrefix: TRAC_NETWORK_MSB_MAINNET_PREFIX,
         addressPrefixLength: TRAC_NETWORK_MSB_MAINNET_PREFIX.length,
         bech32mHrpLength: TRAC_NETWORK_MSB_MAINNET_PREFIX.length + 1, // len(addressPrefix + separator)
-        bootstrap: '78c8cd6e27762911c5e0553a881d0acd726093908bbd38e36e53132c36abe50c',
+        bootstrap: '12f7f1668eac2e691e17cbc6a53e509c5cee78cdcac562313091c64e5fd077d6',
         channel: '12312313123123',
         dhtBootstrap: ['116.202.214.149:10001','157.180.12.214:10001','node1.hyperdht.org:49737','node2.hyperdht.org:49737','node3.hyperdht.org:49737'],
         derivationPath: address.MAINNET_DERIVATION_PATH,
